d4b5cc04359bc1d8e798d2049a6f1b0679a8cb04
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-10-13 18:22:32 +0200

    Refinada la clase Usario #18 (avanza #3)

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,5 +1,22 @@
-interface IUsuario {
-    nombre: string,
-    email: string,
-    password: string
+import {Entity, PrimaryGeneratedColumn, Column, ManyToMany, OneToMany} from "typeorm";
+import {Partido} from "./partido.model";
+
+@Entity()
+export class Usuario{
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    nombre!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    password!: string;
+
+    @OneToMany(() => Partido, partido => partido.usuario)
+    partidos: Partido[];
+
 }
