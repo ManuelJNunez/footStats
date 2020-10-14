06f5092aaeb84d5cdeb8ebe70b854215ef019f07
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-10-15 00:35:42 +0200

    Añadidos metodos de la clase Partido #18

--- src/models/partido.model.ts
+++ src/models/partido.model.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToMany, ManyToOne} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne} from "typeorm";
 import {Usuario} from "./usuario.model";
 
 export enum Resultado {
@@ -11,26 +11,53 @@ export enum Tipo {
     Defensa
 }
 
+export interface IJugada {
+    momento: Date,
+    jugada: Tipo,
+    resultado: Resultado,
+    comentario: string
+}
+
 @Entity()
-export class Partido{
+export class Partido extends BaseEntity{
     
     @PrimaryGeneratedColumn()
-    id!: number;
+    id: number;
 
     @Column()
-    fechaIni!: Date;
+    horaIni: Date;
 
     @Column()
-    fechaFin!: Date;
+    horaFin: Date;
 
-    @Column()
-    jugadas!: [{
-        jugada: Tipo,
-        resultado: Resultado,
-        comentario: string
-    }];
+    @Column("simple-json")
+    jugadas: IJugada[];
 
     @ManyToOne(() => Usuario, usuario => usuario.partidos)
-    usuario!: Usuario;
+    usuario: Usuario;
 
+    constructor(horaIni: Date, horaFin: Date, usuario: Usuario) {
+        super();
+
+        this.id = 0;
+        this.horaIni = horaIni;
+        this.horaFin = horaFin;
+        this.jugadas = [];
+        this.usuario = usuario;
+    }
+
+    public addJugada(jugada: IJugada): void {
+        this.jugadas.push(jugada);
+    }
+
+    public removeJugada(jugada: IJugada): boolean {
+        let index = this.jugadas.indexOf(jugada);
+
+        if(index > -1) {
+            this.jugadas.splice(index);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
