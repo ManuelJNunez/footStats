c9e5afe91c06033cff96cab20a0e097c78a3a96a
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-10-15 12:11:16 +0200

    Arreglada la sintaxis y estilo de todas los ficheros de TS closes #24

--- tests/test_partido.ts
+++ tests/test_partido.ts
@@ -1,77 +1,75 @@
-import {expect} from "chai";
-import {Usuario} from "../src/models/usuario.model";
-import {Partido, Tipo, Resultado} from "../src/models/partido.model";
+import { expect } from 'chai'
+import { Usuario } from '../src/models/usuario.model'
+import { Partido, Tipo, Resultado } from '../src/models/partido.model'
 
-let usuario = new Usuario('Manuel', 'manueljesusnunezruiz@gmail.com', '1234');
-const horaIni = new Date(2020, 9, 14, 18);
-const horaFin = new Date(2020, 9, 14, 18, 45);
-let partido = new Partido(horaIni, horaFin, usuario);
+const usuario = new Usuario('Manuel', 'manueljesusnunezruiz@gmail.com', '1234')
+const horaIni = new Date(2020, 9, 14, 18)
+const horaFin = new Date(2020, 9, 14, 18, 45)
+const partido = new Partido(horaIni, horaFin, usuario)
 
-describe('Tests de construcción y alteración del objeto Partido', function(){
+describe('Tests de construcción y alteración del objeto Partido', function () {
+  it('Debería construir de forma correcta el objeto Partido', function () {
+    expect(partido.horaIni).to.be.equal(horaIni)
+    expect(partido.horaFin).to.be.equal(horaFin)
+    expect(partido.jugadas).to.have.lengthOf(0)
+    expect(partido.usuario).to.be.equal(usuario)
+  })
 
-    it('Debería construir de forma correcta el objeto Partido', function(){
-        expect(partido.horaIni).to.be.equal(horaIni);
-        expect(partido.horaFin).to.be.equal(horaFin);
-        expect(partido.jugadas).to.have.lengthOf(0);
-        expect(partido.usuario).to.be.equal(usuario);
-    })
+  it('Debería actualizar el valor de la variable correctamente', function () {
+    const nuevaFecha = new Date(2020, 9, 14, 17, 50)
 
-    it('Debería actualizar el valor de la variable correctamente', function(){
-        let nuevaFecha = new Date(2020, 9, 14, 17, 50);
+    partido.horaIni = nuevaFecha
 
-        partido.horaIni = nuevaFecha;
+    expect(partido.horaIni).to.be.equal(nuevaFecha)
+  })
 
-        expect(partido.horaIni).to.be.equal(nuevaFecha);
-    })
-
-    it('Debería de lanzar una excepción', function(){
-        expect(() => new Partido(horaFin, horaIni, usuario)).to.throw('Hora de inicio mayor que hora de fin');
-    })
+  it('Debería de lanzar una excepción', function () {
+    expect(() => new Partido(horaFin, horaIni, usuario)).to.throw('Hora de inicio mayor que hora de fin')
+  })
 })
 
-describe('Tests para añadir/eliminar jugadas de un partido', function(){
-    let jugada = {
-        momento: new Date(2020, 9, 14, 18, 10),
-        jugada: Tipo.Ataque,
-        resultado: Resultado.Acertado,
-        comentario: 'El número 12 ha fallado un tiro desde fuera del área'
+describe('Tests para añadir/eliminar jugadas de un partido', function () {
+  const jugada = {
+    momento: new Date(2020, 9, 14, 18, 10),
+    jugada: Tipo.Ataque,
+    resultado: Resultado.Acertado,
+    comentario: 'El número 12 ha fallado un tiro desde fuera del área'
+  }
+
+  it('Debería de añadir una nueva jugada al partido', function () {
+    partido.addJugada(jugada)
+
+    expect(partido.jugadas).to.have.lengthOf(1)
+  })
+
+  it('Debería de tener los parámetros inicializados de manera correcta', function () {
+    expect(partido.jugadas[0].momento).to.be.equal(jugada.momento)
+    expect(partido.jugadas[0].jugada).to.be.equal(jugada.jugada)
+    expect(partido.jugadas[0].resultado).to.be.equal(jugada.resultado)
+    expect(partido.jugadas[0].comentario).to.be.equal(jugada.comentario)
+  })
+
+  it('Debería de eliminarse la jugada del partido', function () {
+    const eliminado = partido.removeJugada(jugada)
+
+    expect(eliminado).to.be.equal(true)
+    expect(partido.jugadas).to.have.lengthOf(0)
+  })
+
+  it('No debería de eliminar nada', function () {
+    const eliminado = partido.removeJugada(jugada)
+
+    expect(eliminado).to.be.equal(false)
+  })
+
+  it('Debería lanzar excepción por introducir valor no correcto', function () {
+    const jugadaIncorrecta = {
+      momento: new Date(Date.now()),
+      jugada: Tipo.Ataque,
+      resultado: Resultado.Acertado,
+      comentario: 'El número 12 ha fallado un tiro desde fuera del área'
     }
 
-    it('Debería de añadir una nueva jugada al partido', function(){
-        partido.addJugada(jugada);
-
-        expect(partido.jugadas).to.have.lengthOf(1);
-    })
-
-    it('Debería de tener los parámetros inicializados de manera correcta', function(){
-        expect(partido.jugadas[0].momento).to.be.equal(jugada.momento);
-        expect(partido.jugadas[0].jugada).to.be.equal(jugada.jugada);
-        expect(partido.jugadas[0].resultado).to.be.equal(jugada.resultado);
-        expect(partido.jugadas[0].comentario).to.be.equal(jugada.comentario);
-    })
-
-    it('Debería de eliminarse la jugada del partido', function(){
-        let eliminado = partido.removeJugada(jugada);
-
-        expect(eliminado).to.be.equal(true);
-        expect(partido.jugadas).to.have.lengthOf(0);
-    })
-
-    it('No debería de eliminar nada', function(){
-        let eliminado = partido.removeJugada(jugada);
-
-        expect(eliminado).to.be.equal(false);
-    })
-
-    it('Debería lanzar excepción por introducir valor no correcto', function(){
-        let jugadaIncorrecta = {
-            momento: new Date(Date.now()),
-            jugada: Tipo.Ataque,
-            resultado: Resultado.Acertado,
-            comentario: 'El número 12 ha fallado un tiro desde fuera del área'
-        }
-
-        expect(partido.addJugada.bind(partido, jugadaIncorrecta)).to.throw('La jugada debe de suceder antes del fin del partido y después del inicio.');
-    })
+    expect(partido.addJugada.bind(partido, jugadaIncorrecta)).to.throw('La jugada debe de suceder antes del fin del partido y después del inicio.')
+  })
 })
-
