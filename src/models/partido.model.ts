5f457038c1d0746899bf8d9b189e2745c412a1e6
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-10-31 17:40:09 +0100

    Documentada la clase Partido #51

--- src/models/partido.model.ts
+++ src/models/partido.model.ts
@@ -1,24 +1,44 @@
+// partido.model.ts
+/**
+ * Este módulo contiene la declaración de la clase que representa a los partidos
+ * @packageDocumentation
+ */
+
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne } from 'typeorm'
 import { Usuario } from './usuario.model'
 import { Jugada } from './jugada.model'
 
+/**
+ * Clase que representa los partidos
+ */
 @Entity()
 export class Partido extends BaseEntity {
+    /** Identificador único del partido */
     @PrimaryGeneratedColumn()
     id: number;
 
+    /** Hora de inicio del partido */
     @Column()
     horaIni: Date;
 
+    /** Hora de finalización del partido */
     @Column()
     horaFin: Date;
 
+    /** Histórico de jugadas que se han producido en el partido */
     @ManyToOne(() => Jugada, jugada => jugada.partido)
     jugadas: Jugada[];
 
+    /** {@link Usuario} que registró el partido */
     @ManyToOne(() => Usuario, usuario => usuario.partidos)
     usuario: Usuario;
 
+    /**
+     * Crea un partido
+     * @param horaIni Hora de inicio del partido nuevo
+     * @param horaFin Hora de finalización del partido nuevo
+     * @param usuario Usuario que ha registrado el partido nuevo
+     */
     constructor (horaIni: Date, horaFin: Date, usuario: Usuario) {
       super()
 
@@ -33,6 +53,10 @@ export class Partido extends BaseEntity {
       this.usuario = usuario
     }
 
+    /**
+     * Añade una jugada nueva al partido
+     * @param jugada Objeto de la clase {@link Jugada} que contiene información sobre la misma
+     */
     public addJugada (jugada: Jugada): void {
       if (!(jugada.momento.getTime() >= this.horaIni.getTime() && jugada.momento.getTime() <= this.horaFin.getTime())) {
         throw new Error('La jugada debe de suceder antes del fin del partido y después del inicio.')
@@ -41,6 +65,11 @@ export class Partido extends BaseEntity {
       this.jugadas.push(jugada)
     }
 
+    /**
+     * Elimina una jugada del partido
+     * @param jugada Objeto de la clase {@link Jugada} que queremos eliminar del partido
+     * @returns Si la operación tuvo éxito o no
+     */
     public removeJugada (jugada: Jugada): boolean {
       const index = this.jugadas.indexOf(jugada)
 
