a56d8a650bd3a12c6f9ee5a8ab3955baf4bf2937
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-10-31 17:40:44 +0100

    Documentada la clase Jugada closes #51

--- src/models/jugada.model.ts
+++ src/models/jugada.model.ts
@@ -1,36 +1,83 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne, OneToMany } from 'typeorm'
+// jugada.model.ts
+/**
+ * Este módulo contiene la declaración de la clase que representa las Jugadas
+ * @packageDocumentation
+ */
+
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, OneToMany } from 'typeorm'
 import { Partido } from './partido.model'
 
+/**
+ * Este enum contiene los posibles valores para los resultados de las jugadas
+ */
 export enum Resultado {
+    /**
+     * Este indica que la jugada ha salido bien
+     */
     Acertado,
+    /**
+     * Este indica que la jugada ha salido mal
+     */
     Fallido
 }
 
+/**
+ * Este enum contiene los posibles valores para los tipos de jugadas
+ */
 export enum TipoJugada {
     Ataque,
     Defensa
 }
 
+/**
+ * Clase que representa las jugadas
+ */
 @Entity()
 export class Jugada extends BaseEntity {
+    /**
+     * Identificador único de la jugada
+     */
     @PrimaryGeneratedColumn()
     id: number;
 
+    /**
+     * Hora a la que sucede la jugada
+     */
     @Column()
     momento: Date;
 
+    /**
+     * Tipo de jugada ver {@link TipoJugada}
+     */
     @Column()
     tipo: TipoJugada;
 
+    /**
+     * Resultado de la jugada ver {@link Resultado}
+     */
     @Column()
     resultado: Resultado;
 
+    /**
+     * Algún comentario adicional sobre la jugada sucedida
+     */
     @Column()
     comentario: string;
 
+    /**
+     * {@link Partido} en el que sucedió la jugada
+     */
     @OneToMany(() => Partido, partido => partido.jugadas)
     partido: Partido;
 
+    /**
+     * Crea una jugada
+     * @param momento Hora a la que sucedió la jugada nueva
+     * @param tipo Tipo de jugada nueva ver {@link TipoJugada}
+     * @param resultado Resultado de la jugada nueva ver {@link Resultado}
+     * @param partido {@link Partido} en el que sucedió la jugada nueva
+     * @param comentario Comentario adicional sobre la jugada nueva
+     */
     constructor (momento: Date, tipo: TipoJugada, resultado: Resultado, partido: Partido, comentario: string) {
       super()
 
