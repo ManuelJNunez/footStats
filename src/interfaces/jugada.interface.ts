9e4c8cb30cf158dc3f7d98000de310fab3dcd531
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-05 13:45:09 +0100

    Añadido prototipo de los objetos JSON de la clase Jugada #63

---
+++ src/interfaces/jugada.interface.ts
@@ -0,0 +1,13 @@
+import { Resultado, TipoJugada } from '../models/jugada.model'
+import { PartidoI } from './partido.interface'
+
+/**
+ * Interfaz que representa a las Jugadas
+ */
+export interface JugadaI {
+    id: number;
+    tipo: TipoJugada;
+    resultado: Resultado;
+    comentario: string;
+    partido: PartidoI;
+}
