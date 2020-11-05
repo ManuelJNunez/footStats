7da965fc3bfb2eb1573cd13f8f1bd67046a5dd2f
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-05 16:31:09 +0100

    Añadidos test para el toJSON de Usuario #63 (avanza #4)

--- tests/usuario.test.ts
+++ tests/usuario.test.ts
@@ -1,9 +1,16 @@
 import { expect } from 'chai'
 import { Usuario } from '../src/models/usuario.model'
 
-describe('Tests de construcción y alteración de objeto Usuario', function () {
-  const usuario = new Usuario('Manuel', 'manueljesusnunezruiz@gmail.com', '1234')
+const usuario = new Usuario('Manuel', 'manueljesusnunezruiz@gmail.com', '1234')
 
+const newPass = '12345'
+const newName = 'Manolo'
+const newEmail = 'mjnunez@correo.ugr.es'
+
+const horaIni = new Date(2020, 10, 14, 18)
+const horaFin = new Date(2020, 10, 14, 18, 45)
+
+describe('Tests de construcción y alteración de objeto Usuario', function () {
   it('Debería de crearse un objeto Usuario con los valores que se le han pasado', function () {
     expect(usuario.id).to.be.equal(0)
     expect(usuario.nombre).to.be.equal('Manuel')
@@ -13,10 +20,6 @@ describe('Tests de construcción y alteración de objeto Usuario', function () {
   })
 
   it('Debería de alterar correctamente los datos del usuario a través de los getters y setters', function () {
-    const newPass = '12345'
-    const newName = 'Manolo'
-    const newEmail = 'mjnunez@correo.ugr.es'
-
     usuario.password = newPass
     usuario.nombre = newName
     usuario.email = newEmail
@@ -28,10 +31,6 @@ describe('Tests de construcción y alteración de objeto Usuario', function () {
 })
 
 describe('Tests para añadir/eliminar partidos a un Usuario', function () {
-  const usuario = new Usuario('Manuel', 'manueljesusnunezruiz@gmail.com', '1234')
-  const horaIni = new Date(2020, 10, 14, 18)
-  const horaFin = new Date(2020, 10, 14, 18, 45)
-
   it('Debería añadir un nuevo objeto al array de partidos del usuario', function () {
     usuario.addPartido(horaIni, horaFin)
 
@@ -41,7 +40,6 @@ describe('Tests para añadir/eliminar partidos a un Usuario', function () {
   it('Debería de tener los parámetros inicializados de manera correcta', function () {
     expect(usuario.partidos[0].horaIni).to.be.equal(horaIni)
     expect(usuario.partidos[0].horaFin).to.be.equal(horaFin)
-    expect(usuario.partidos[0].usuario).to.be.equal(usuario)
     expect(usuario.partidos[0].jugadas).to.have.lengthOf(0)
   })
 
@@ -56,5 +54,19 @@ describe('Tests para añadir/eliminar partidos a un Usuario', function () {
     const eliminado = usuario.removePartido(usuario.partidos[0])
 
     expect(eliminado).to.be.equal(false)
+  })
+})
+
+describe('Tests del toJSON de la clase Usuario', function () {
+  it('Debería de devolver el objeto JSON con la información del usuario', function () {
+    usuario.addPartido(horaIni, horaFin)
+
+    const usuariojson = usuario.toJSON()
+
+    expect(usuariojson.id).to.be.equal(usuario.id)
+    expect(usuariojson.nombre).to.be.equal(usuario.nombre)
+    expect(usuariojson.email).to.be.equal(usuario.email)
+    expect(usuariojson).to.not.have.property('password')
+    expect(usuariojson.partidos).to.have.length(1)
   })
 })
