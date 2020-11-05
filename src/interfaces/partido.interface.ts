a1a5f481f1af4e5ec5a747ed03e90bab74dc02a7
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-05 16:12:49 +0100

    Comentada interfaz Partido #63

--- src/interfaces/partido.interface.ts
+++ src/interfaces/partido.interface.ts
@@ -1,9 +1,15 @@
-import { UsuarioI } from './usuario.interface'
 import { JugadaI } from './jugada.interface'
 
+/**
+ * Interfaz que representa los partidos
+ */
 export interface PartidoI {
+    /** Identificador único del Partido */
     id: number;
+    /** Hora de inicio del Partido */
     horaIni: Date;
+    /** Hora de finalización del partido */
     horaFin: Date;
+    /** Objetivos del tipo {@link JugadaI} que han sucede en el Partido */
     jugadas: JugadaI[];
 }
