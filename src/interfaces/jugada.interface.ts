5d477f38f163fcfa08a51d999b3158c8f1e4440b
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-05 16:12:33 +0100

    Comentada interfaz Jugada #63

--- src/interfaces/jugada.interface.ts
+++ src/interfaces/jugada.interface.ts
@@ -4,8 +4,12 @@ import { Resultado, TipoJugada } from '../models/jugada.model'
  * Interfaz que representa a las Jugadas
  */
 export interface JugadaI {
+    /** Identificador único de la jugada */
     id: number;
+    /** Tipo de jugada (Ataque o defensa) */
     tipo: TipoJugada;
+    /** Resultado de la jugada (acertada o fallida) */
     resultado: Resultado;
+    /** Comentario adicional sobre la jugada */
     comentario: string;
 }
