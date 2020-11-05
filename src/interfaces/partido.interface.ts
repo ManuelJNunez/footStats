0ee540046a5b8831754be58b44944dee67d051dc
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-05 13:44:54 +0100

    Añadido prototipo de los objetos JSON de la clase Partido #63

---
+++ src/interfaces/partido.interface.ts
@@ -0,0 +1,10 @@
+import { UsuarioI } from './usuario.interface'
+import { JugadaI } from './jugada.interface'
+
+export interface PartidoI {
+    id: number;
+    horaIni: Date;
+    horaFin: Date;
+    jugadas: JugadaI[];
+    usuario: UsuarioI;
+}
