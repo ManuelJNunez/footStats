38ac3da73be980dd38f5f50f647161764d9cacc9
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-05 16:12:06 +0100

    Comentada interfaz Usuario #63

--- src/interfaces/usuario.interface.ts
+++ src/interfaces/usuario.interface.ts
@@ -4,9 +4,14 @@ import { PartidoI } from './partido.interface'
  * Interfaz que representa al Usuario
  */
 export interface UsuarioI {
+    /** Identificador único del usuario */
     id: number;
+    /** Nombre del usuario */
     nombre: string;
+    /** Dirección de correo electrónico del usuario */
     email: string;
+    /** Contraseña del usuario */
     password?: string;
+    /** Objetos del tipo {@link Partido} registrados por el usuario */
     partidos: PartidoI[];
 }
