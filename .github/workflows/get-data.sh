255d62ba67a89170434924600cad75adcae06eb8
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-20 19:08:51 +0100

    Fix: cambio e-mail en get-data.sh #69

--- .github/workflows/get-data.sh
+++ .github/workflows/get-data.sh
@@ -6,7 +6,7 @@ curl --request GET \
 	--header "x-rapidapi-key: $RAPIDAPI_KEY" \
     --output functions/data.json
 
-git config --local user.email "manueljesusnunezruiz@gmail.com"
+git config --local user.email "action@github.com"
 git config --local user.name "LaLiga-Bot"
 git add functions/data.json
 git commit -m "Actualizados los datos de LaLiga [ci skip]"
