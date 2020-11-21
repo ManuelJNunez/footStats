2309c48b230b33c738a1170414ba4b530e9b31f1
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-21 16:11:03 +0100

    Cambio data.js a data.json en get-data #69

--- .github/workflows/get-data.sh
+++ .github/workflows/get-data.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo "module.exports.data = " > functions/data.js
-
-mkdir tmp
-
 curl --request GET \
 	--url https://api-football-v1.p.rapidapi.com/v2/leagueTable/2833 \
 	--header "x-rapidapi-host: api-football-v1.p.rapidapi.com" \
 	--header "x-rapidapi-key: $RAPIDAPI_KEY" \
-    --output tmp/data.json
-
-echo `cat tmp/data.json` >> functions/data.js
-
-rm -rf tmp/
+    --output functions/data.json
