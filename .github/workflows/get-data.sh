d5d43b5f63b582888ecfa9e5221dcb92895fd0dd
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-21 12:54:34 +0100

    Fix get-data.sh #69

--- .github/workflows/get-data.sh
+++ .github/workflows/get-data.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "module.exports.data = " > functions/data.js
+
+mkdir tmp
+
 curl --request GET \
 	--url https://api-football-v1.p.rapidapi.com/v2/leagueTable/2833 \
 	--header "x-rapidapi-host: api-football-v1.p.rapidapi.com" \
 	--header "x-rapidapi-key: $RAPIDAPI_KEY" \
-    --output functions/data.js
+    --output tmp/data.json
+
+echo `cat tmp/data.json` >> functions/data.js
