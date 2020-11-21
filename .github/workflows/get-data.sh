5c8d7639a62d2b94ab4e7d5b43ee1995e9184e20
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-21 10:52:22 +0100

    Cambiado output curl get-data.sh #69

--- .github/workflows/get-data.sh
+++ .github/workflows/get-data.sh
@@ -4,4 +4,4 @@ curl --request GET \
 	--url https://api-football-v1.p.rapidapi.com/v2/leagueTable/2833 \
 	--header "x-rapidapi-host: api-football-v1.p.rapidapi.com" \
 	--header "x-rapidapi-key: $RAPIDAPI_KEY" \
-    --output functions/data.json
+    --output functions/data.js
