9fc2e5f7fe9efdb939c6b7a7a8a04a900f3f0050
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-22 12:03:11 +0100

    Añado primera version del bot #72

---
+++ telegram/functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions')
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(functions.config().telegram.key)
+
+bot.start(ctx => ctx.reply('Hola, probando'))
+
+exports.bot = functions
+  .region('europe-west1')
+  .https.onRequest((req, res) =>
+    bot.handleUpdate(req.body, res).then((rv) => !rv && res.sendStatus(200))
+  )
