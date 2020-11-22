5657613283b05f41abed942e9c7fa23eefb4bd88
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-22 12:59:45 +0100

    Añade comando liga para Telegram #72

--- telegram/functions/index.js
+++ telegram/functions/index.js
@@ -1,9 +1,29 @@
 const functions = require('firebase-functions')
 const Telegraf = require('telegraf')
+const axios = require('axios')
 
 const bot = new Telegraf(functions.config().telegram.key)
 
 bot.start(ctx => ctx.reply('Bienvenido a footStats BOT. Para obtener ayuda use el comando /help.'))
+
+bot.command('liga', (ctx) => {
+  let response = 'La tabla de clasificación de La Liga es la siguiente:\n'
+
+  axios({
+    method: 'get',
+    url: `${functions.config().footstats.url}/league`,
+    responseType: 'json'
+  })
+    .then(function (response) {
+      const data = response.data
+
+      for (const team of data.table) {
+        response += `${team.rank}. ${team.teamName} (**${team.points} ptos.**)\n`
+      }
+
+      ctx.reply(response)
+    })
+})
 
 exports.bot = functions
   .region('europe-west1')
