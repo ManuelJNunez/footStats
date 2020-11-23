e36f87b188116bed57790b7fc9ecfd68e1d7fed2
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-23 17:19:15 +0100

    Añade headers

--- functions/status.js
+++ functions/status.js
@@ -2,6 +2,9 @@
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({ status: 'Healthy' })
   }
 }
