664ba64e9249544efcf42b4fbfe781393478393b
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-23 17:22:11 +0100

    Añade headers a credits close #75

--- functions/credits.js
+++ functions/credits.js
@@ -5,6 +5,9 @@ exports.handler = async function (event, context) {
 
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify(response)
   }
 }
