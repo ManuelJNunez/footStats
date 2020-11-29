dec54759ef21c0b6aaee281735c12b2688bfcd23
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-11-29 22:35:58 +0100

    El script ahora hace update tambien en el bot de Telegram #76

--- .github/workflows/get-data.sh
+++ .github/workflows/get-data.sh
@@ -5,3 +5,5 @@ curl --request GET \
 	--header "x-rapidapi-host: api-football-v1.p.rapidapi.com" \
 	--header "x-rapidapi-key: $RAPIDAPI_KEY" \
     --output functions/data.json
+
+cp -a functions/data.json telegram/functions/data.json
