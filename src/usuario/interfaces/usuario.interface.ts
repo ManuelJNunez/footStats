dafe76369dddf0882a9020dbc8607ef77ec3ae18
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-07 13:20:44 +0100

    Refactor: cambio path usuario.interface

---
+++ src/usuario/interfaces/usuario.interface.ts
@@ -0,0 +1,17 @@
+import { PartidoI } from '../../interfaces/partido.interface';
+
+/**
+ * Interfaz que representa al Usuario
+ */
+export interface UsuarioI {
+  /** Identificador único del usuario */
+  id: number;
+  /** Nombre del usuario */
+  nombre: string;
+  /** Dirección de correo electrónico del usuario */
+  email: string;
+  /** Contraseña del usuario */
+  password?: string;
+  /** Objetos del tipo {@link Partido} registrados por el usuario */
+  partidos: PartidoI[];
+}
