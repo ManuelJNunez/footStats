942bd8977b4fddcd75809810fe987b3f021c4d64
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-07 13:20:13 +0100

    Añade endpoints para Usuario #3 #4

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,9 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsuarioService } from './usuario.service';
 
-@Controller('usuario')
+@Controller('user')
 export class UsuarioController {
+  constructor(private readonly usuarioService: UsuarioService) {}
+
   @Get()
-  getHello(): any {
-    return { status: 'Healthy' };
+  getUser(@Body('email') email: string) {
+    return this.usuarioService.findByEmail(email);
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() user: CreateUserDto) {
+    return {
+      message: 'Usuario registrado con éxito',
+      user: this.usuarioService.create(user),
+    };
+  }
+
+  @Put(':id')
+  @UsePipes(new ValidationPipe())
+  updateUser(@Param('id') id: number, @Body() user: CreateUserDto) {
+    return {
+      message: 'Usuario modificado con éxito',
+      user: this.usuarioService.update(id, user),
+    };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    return {
+      message: 'Usuario eliminado con éxito',
+      user: this.usuarioService.delete(id),
+    };
   }
 }
