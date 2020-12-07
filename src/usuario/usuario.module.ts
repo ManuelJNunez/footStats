d7ec44c241dfc3ce7ba4bf84e93ffa74bf97d7a0
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-07 13:18:31 +0100

    Añade modulo Usuario

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsuarioController } from './usuario.controller';
+import { UsuarioService } from './usuario.service';
 
 @Module({
   controllers: [UsuarioController],
+  providers: [UsuarioService],
 })
 export class UsuarioModule {}
