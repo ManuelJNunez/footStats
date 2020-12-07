a0795acd9d2836012dfb3e176aa3243ba311819f
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-07 13:05:37 +0100

    Añade modulos creados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { PartidoModule } from './partido/partido.module';
 import { JugadaModule } from './jugada/jugada.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsuarioModule, PartidoModule, JugadaModule],
+  imports: [UsuarioModule, PartidoModule, JugadaModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
