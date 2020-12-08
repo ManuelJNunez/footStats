8101cc1315cb93b2c765e7393b7da16f3019356a
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-08 13:08:55 +0100

    Añadido modulo ConfigModule para poder leer del .env

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   controllers: [UsuarioController],
   providers: [UsuarioService],
   exports: [UsuarioService],
