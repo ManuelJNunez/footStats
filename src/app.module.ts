1e843a2ee68d566d137966943d54af91ee960313
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-08 13:08:11 +0100

    Actualizados los imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
 import { PartidoModule } from './partido/partido.module';
 import { JugadaModule } from './jugada/jugada.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsuarioModule, PartidoModule, JugadaModule, AuthModule],
+  imports: [UsuarioModule, PartidoModule, JugadaModule],
   controllers: [AppController],
   providers: [],
 })
