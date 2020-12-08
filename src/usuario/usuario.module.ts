7bf986861cd16241d4262908405c261e0864b7ae
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-08 17:27:45 +0100

    Refactor

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
   controllers: [UsuarioController],
   providers: [UsuarioService],
-  exports: [UsuarioService],
 })
 export class UsuarioModule {}
