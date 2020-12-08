edc7c95fc8c6afc34529f1b644575709a57e8376
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-08 17:26:29 +0100

    Fix: añade provider al TestingModule #3 #4

--- src/usuario/usuario.controller.spec.ts
+++ src/usuario/usuario.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsuarioController } from './usuario.controller';
+import { UsuarioService } from './usuario.service';
 
 describe('UsuarioController', () => {
   let controller: UsuarioController;
@@ -7,6 +8,7 @@ describe('UsuarioController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsuarioController],
+      providers: [UsuarioService],
     }).compile();
 
     controller = module.get<UsuarioController>(UsuarioController);
