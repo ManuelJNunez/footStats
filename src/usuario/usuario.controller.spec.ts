3dfb74203924834af9618a7159174696f05450ba
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-09 12:51:29 +0100

    Añade test unitarios para login endpoint #4

--- src/usuario/usuario.controller.spec.ts
+++ src/usuario/usuario.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { LoginDTO } from './dto/login.dto';
 import { UsuarioController } from './usuario.controller';
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
@@ -20,6 +21,11 @@ describe('UsuarioController', () => {
     password: '1234',
     nickname: 'manuel',
   } as CreateUserDTO;
+
+  const loginDto = {
+    email: 'mjnunez@gmail.com',
+    password: '1234',
+  } as LoginDTO;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -81,5 +87,18 @@ describe('UsuarioController', () => {
     expect(res.user).toEqual(userObj);
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith(id);
+  });
+
+  it('should retrieve an object with the token', () => {
+    const token = 'ThisTokenIsValid';
+    const spy = jest.spyOn(service, 'generarToken');
+    spy.mockReturnValueOnce(token);
+
+    const res = controller.login(loginDto);
+
+    expect(res.message).toEqual('Logeado con éxito');
+    expect(res.token).toEqual(token);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(loginDto);
   });
 });
