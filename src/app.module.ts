3d8bd52724e166585a51edc6c9f9ed3f2155bff5
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-09 20:08:32 +0100

    Aplica el middleware de logging en todas las rutas #28

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
 import { JugadaController } from './jugada/jugada.controller';
 import { JugadaModule } from './jugada/jugada.module';
 import { PartidoModule } from './partido/partido.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsuarioModule, JugadaModule, PartidoModule],
   controllers: [AppController, JugadaController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
