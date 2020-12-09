e06db33929e38523fe8370ccb7b03fcb5ca27760
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-09 20:31:17 +0100

    Añade tests para el middleware de logging #28

--- src/common/middleware/logger.middleware.spec.ts
+++ src/common/middleware/logger.middleware.spec.ts
@@ -1,7 +1,30 @@
+import { Logger } from '@nestjs/common';
 import { LoggerMiddleware } from './logger.middleware';
 
 describe('LoggerMiddleware', () => {
   it('should be defined', () => {
     expect(new LoggerMiddleware()).toBeDefined();
+  });
+
+  it('Should call next and the logger', () => {
+    const req = {
+      method: 'method',
+      url: '/somepath',
+    };
+
+    const res = {
+      statusCode: 200,
+    };
+
+    const next = jest.fn();
+    const logMock = (Logger.prototype.log = jest.fn());
+
+    new LoggerMiddleware().use(req, res, next);
+
+    expect(next).toBeCalledTimes(1);
+    expect(logMock).toBeCalledTimes(1);
+    expect(logMock).toBeCalledWith(
+      `Method: '${req.method}' PATH: '${req.url}' STATUS: '${res.statusCode}'`,
+    );
   });
 });
