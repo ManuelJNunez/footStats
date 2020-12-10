a61cf414df9454fda6b572eb55342c88645d9345
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-10 13:44:17 +0100

    Añade tests de integracion para /user #3 #4

---
+++ test/usuario.e2e-spec.ts
@@ -0,0 +1,108 @@
+import { Test } from '@nestjs/testing';
+import { UsuarioModule } from '../src/usuario/usuario.module';
+import { UsuarioService } from '../src/usuario/usuario.service';
+import { ExecutionContext, INestApplication } from '@nestjs/common';
+import { AuthGuard } from '../src/common/guards/auth.guard';
+const request = require('supertest');
+
+describe('Usuarios', () => {
+  let app: INestApplication;
+  const usuarioService = {
+    create: (user) => {
+      return 'test';
+    },
+    findByEmail: (email) => {
+      return 'found';
+    },
+    update: (id, user) => {
+      return `updating ${id}`;
+    },
+    delete: (id, user) => {
+      return `deleting ${id}`;
+    },
+    generarToken: (login) => {
+      return `aValidToken`;
+    },
+  };
+
+  const user = {
+    email: 'manueljesusnunezruiz@gmail.com',
+    password: '1234',
+    nickname: 'mjnunez',
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [UsuarioModule],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate: (context: ExecutionContext) => true })
+      .overrideProvider(UsuarioService)
+      .useValue(usuarioService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('POST /user', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send(user)
+      .expect(201)
+      .expect({
+        message: 'Usuario registrado con éxito',
+        user: usuarioService.create(user),
+      });
+  });
+
+  it('GET /user', () => {
+    return request(app.getHttpServer())
+      .get('/user')
+      .send(user.email)
+      .expect(200, usuarioService.findByEmail(user.email));
+  });
+
+  it('PUT /user/:id', () => {
+    const id = 0;
+
+    return request(app.getHttpServer())
+      .put(`/user/${id}`)
+      .send(user)
+      .expect(200, {
+        message: 'Usuario modificado con éxito',
+        user: usuarioService.update(id, user),
+      });
+  });
+
+  it('DELETE /user/:id', () => {
+    const id = 0;
+
+    return request(app.getHttpServer())
+      .delete(`/user/${id}`)
+      .send(user)
+      .expect(200, {
+        message: 'Usuario eliminado con éxito',
+        user: usuarioService.delete(id, user),
+      });
+  });
+
+  it('POST /user/login', () => {
+    const login = {
+      email: user.email,
+      password: user.password,
+    };
+
+    return request(app.getHttpServer())
+      .post(`/user/login`)
+      .send(login)
+      .expect(201, {
+        message: 'Logeado con éxito',
+        user: usuarioService.generarToken(login),
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
