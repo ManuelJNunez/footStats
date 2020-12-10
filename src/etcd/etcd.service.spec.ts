e3c06a632714d97d57fb8b9fc2aaa3e2e5ed687b
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-10 19:53:14 +0100

    Añade tests del servicio de configuracion distribuida #84

---
+++ src/etcd/etcd.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Etcd3 } from 'etcd3';
+import { EtcdService } from './etcd.service';
+
+describe('EtcdService', () => {
+  let service: EtcdService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [EtcdService],
+    }).compile();
+
+    service = module.get<EtcdService>(EtcdService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should works with etcd', async () => {
+    const spy = (Etcd3.prototype.get = jest.fn());
+    const key = 'hello';
+
+    spy.mockReturnValueOnce('hi');
+
+    const result = await service.get(key);
+
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(key);
+    expect(result).toBe(result);
+  });
+
+  it('should works with process environment', async () => {
+    const spy = (Etcd3.prototype.get = jest.fn());
+    const key = 'hello';
+    const expected = 'hi';
+    process.env.hello = expected;
+
+    spy.mockImplementationOnce((key) => {
+      throw new Error();
+    });
+
+    const result = await service.get(key);
+    expect(spy).toBeCalledWith(key);
+    expect(result).toBe(expected);
+  });
+});
