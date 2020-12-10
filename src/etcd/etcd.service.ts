a82ff753b4a3cb1d2b822bf26d4879e6e24376d0
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-10 19:52:55 +0100

    Añade servicio de configuracion distribuida #84

---
+++ src/etcd/etcd.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Etcd3 } from 'etcd3';
+
+@Injectable()
+export class EtcdService {
+  private client: Etcd3 = new Etcd3({
+    hosts: `${process.env['ETCD_HOST']}:${process.env['ETCD_PORT']}`,
+  });
+
+  async get(key: string) {
+    let res;
+
+    try {
+      res = await this.client.get(key);
+    } catch (error) {
+      res = process.env[key];
+    }
+
+    return res;
+  }
+}
