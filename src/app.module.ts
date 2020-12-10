53a4ce70f03aafde6077a7841ad17cb1e63eafa8
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-10 19:53:56 +0100

    Inyecta dependencia en app.module close #84

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,21 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
-import { JugadaController } from './jugada/jugada.controller';
 import { JugadaModule } from './jugada/jugada.module';
 import { PartidoModule } from './partido/partido.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { EtcdService } from './etcd/etcd.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsuarioModule, JugadaModule, PartidoModule],
-  controllers: [AppController, JugadaController],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    UsuarioModule,
+    JugadaModule,
+    PartidoModule,
+    ConfigModule,
+  ],
+  controllers: [AppController],
+  providers: [EtcdService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
