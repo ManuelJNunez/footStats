58411eb2ac291261e26bc33f596095a65fb91315
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-11 17:56:16 +0100

    Añade LoggerInterceptor a la App #28

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
+  app.useGlobalInterceptors(new LoggerInterceptor());
   await app.listen(3000);
 }
 bootstrap();
