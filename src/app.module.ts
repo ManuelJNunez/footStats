aacfd4c4678f697638bdd42f05228c7fa478da64
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-11 17:55:55 +0100

    Elimina logger middleware #28

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
 import { JugadaModule } from './jugada/jugada.module';
 import { PartidoModule } from './partido/partido.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { EtcdService } from './etcd/etcd.service';
 
 @Module({
@@ -18,8 +17,4 @@ import { EtcdService } from './etcd/etcd.service';
   controllers: [AppController],
   providers: [EtcdService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
