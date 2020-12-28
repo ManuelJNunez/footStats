c58becd11c843bc11dc641e361dede0cbf46ac70
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-28 11:15:36 +0100

    refactor: añade EtcdService en UsuarioModule

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EtcdService } from 'src/etcd/etcd.service';
 import { AuthGuard } from '../common/guards/auth.guard';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
 
 @Module({
   controllers: [UsuarioController],
-  providers: [UsuarioService, AuthGuard],
+  providers: [UsuarioService, EtcdService, AuthGuard],
 })
 export class UsuarioModule {}
