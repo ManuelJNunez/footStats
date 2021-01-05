ddc073a7c4af49ede3eb882a6bbe00c878c507ec
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-05 16:22:41 +0100

    chore: añade directorio database a gitignore :see_no_evil:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { KnexModule } from 'nestjs-knex';
 import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
 import { JugadaModule } from './jugada/jugada.module';
@@ -9,6 +10,19 @@ import { EtcdService } from './etcd/etcd.service';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    KnexModule.forRoot({
+      config: {
+        client: 'pg',
+        // debug: true,
+        connection: {
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          port: parseInt(process.env.DB_PORT),
+        },
+      },
+    }),
     UsuarioModule,
     JugadaModule,
     PartidoModule,
