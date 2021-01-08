a3c6db4b9770c6833de3bdac2119282404ca949d
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-08 21:10:16 +0100

    refactor: elimina knex, voy a usar la libreria pg #87

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { KnexModule } from 'nestjs-knex';
 import { AppController } from './app.controller';
 import { UsuarioModule } from './usuario/usuario.module';
 import { JugadaModule } from './jugada/jugada.module';
 import { PartidoModule } from './partido/partido.module';
 import { EtcdService } from './etcd/etcd.service';
+import { PgModule } from './pg/pg.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    KnexModule.forRoot({
-      config: {
-        client: 'pg',
-        // debug: true,
-        connection: {
-          host: process.env.DB_HOST,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-          port: parseInt(process.env.DB_PORT),
-        },
-      },
-    }),
-    UsuarioModule,
-    JugadaModule,
-    PartidoModule,
-    ConfigModule,
-  ],
+  imports: [UsuarioModule, JugadaModule, PartidoModule, ConfigModule, PgModule],
   controllers: [AppController],
   providers: [EtcdService],
 })
