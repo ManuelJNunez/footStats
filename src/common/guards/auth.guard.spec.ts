90bc2eb45138e53141fb3d3d1d4c5e8674061855
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-09 13:38:26 +0100

    test: actualiza tests (#4 #87)

--- src/common/guards/auth.guard.spec.ts
+++ src/common/guards/auth.guard.spec.ts
@@ -1,5 +1,7 @@
 import { createMock } from '@golevelup/nestjs-testing';
 import { ExecutionContext } from '@nestjs/common';
+import { Pool } from 'pg';
+import { UsuarioI } from '../../usuario/interfaces/usuario.interface';
 import { AuthGuard } from './auth.guard';
 const jwt = require('jsonwebtoken');
 
@@ -10,18 +12,32 @@ const etcdService = {
   getClient: jest.fn(),
 } as any;
 
+const user = {
+  userId: 0,
+  nickname: 'mjnunez',
+  email: 'manueljesusnunezruiz@gmail.com',
+} as UsuarioI;
+
 describe('AuthGuard', () => {
+  let mockQuery;
+  let context;
+
   beforeEach(() => {
+    context = createMock<ExecutionContext>();
+    mockQuery = Pool.prototype.query = jest.fn();
     etcdService.get.mockReturnValueOnce(password);
     etcdService.get.mockClear();
   });
 
-  /*it('should be defined', () => {
-    expect(new AuthGuard(etcdService)).toBeDefined();
+  afterEach(() => {
+    mockQuery.mockClear();
+  });
+
+  it('should be defined', () => {
+    expect(new AuthGuard(etcdService, new Pool())).toBeDefined();
   });
 
   it('should accept the context', async () => {
-    const context = createMock<ExecutionContext>();
     const spy = jest.spyOn(context.switchToHttp(), 'getRequest');
     const token = 'aVeryValidToken';
 
@@ -32,21 +48,55 @@ describe('AuthGuard', () => {
     });
 
     const spyJwt = jest.spyOn(jwt, 'verify');
-    spyJwt.mockReturnValueOnce('verified :D');
+    spyJwt.mockReturnValueOnce(user);
+    mockQuery.mockResolvedValueOnce({ rows: [user], rowCount: 1 });
 
-    const result = await new AuthGuard(etcdService).canActivate(context);
+    const result = await new AuthGuard(etcdService, new Pool()).canActivate(
+      context,
+    );
 
     expect(result).toBe(true);
     expect(spyJwt).toBeCalledTimes(1);
     expect(spyJwt).toBeCalledWith(token, password);
     expect(etcdService.get).toBeCalledTimes(1);
+    expect(mockQuery).toBeCalledWith(
+      `SELECT * FROM users WHERE "userId" = '${user.userId}'`,
+    );
   });
 
-  it('should reject because there is no Bearer in the token', async () => {
-    const context = createMock<ExecutionContext>();
+  it('should reject because authorization is not defined', async () => {
+    const spy = jest.spyOn(context.switchToHttp(), 'getRequest');
+
+    spy.mockReturnValueOnce({
+      headers: {},
+    });
+
+    const result = await new AuthGuard(etcdService, new Pool()).canActivate(
+      context,
+    );
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should reject because there is no token', async () => {
+    const spy = jest.spyOn(context.switchToHttp(), 'getRequest');
+
+    spy.mockReturnValueOnce({
+      headers: {
+        authorization: `Bearer`,
+      },
+    });
+
+    const result = await new AuthGuard(etcdService, new Pool()).canActivate(
+      context,
+    );
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should reject because the authorization field is invalid', async () => {
     const spy = jest.spyOn(context.switchToHttp(), 'getRequest');
     const token = 'aVeryValidToken';
-    process.env.JWT_SECRET = 'aSecretPasssowrd';
 
     spy.mockReturnValueOnce({
       headers: {
@@ -54,18 +104,53 @@ describe('AuthGuard', () => {
       },
     });
 
-    const result = await new AuthGuard(etcdService).canActivate(context);
+    const result = await new AuthGuard(etcdService, new Pool()).canActivate(
+      context,
+    );
 
-    expect(result).toBe(false);
-    expect(etcdService.get).toBeCalledTimes(1);
+    expect(result).toBeFalsy();
   });
 
-  it('should reject because there is no token', async () => {
-    const context = createMock<ExecutionContext>();
+  it('should reject because verify throws an exception', async () => {
+    const spy = jest.spyOn(context.switchToHttp(), 'getRequest');
+    const token = 'aVeryValidToken';
 
-    const result = await new AuthGuard(etcdService).canActivate(context);
+    spy.mockReturnValueOnce({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
 
-    expect(result).toBe(false);
-    expect(etcdService.get).toBeCalledTimes(0);
-  });*/
+    const spyJwt = jest.spyOn(jwt, 'verify');
+    spyJwt.mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const result = await new AuthGuard(etcdService, new Pool()).canActivate(
+      context,
+    );
+
+    expect(result).toBeFalsy();
+  });
+
+  it("should reject because the user doesn't exist", async () => {
+    const spy = jest.spyOn(context.switchToHttp(), 'getRequest');
+    const token = 'aVeryValidToken';
+
+    spy.mockReturnValueOnce({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    const spyJwt = jest.spyOn(jwt, 'verify');
+    spyJwt.mockReturnValueOnce(user);
+    mockQuery.mockReturnValueOnce({ rows: [], rowCount: 0 });
+
+    const result = await new AuthGuard(etcdService, new Pool()).canActivate(
+      context,
+    );
+
+    expect(result).toBeFalsy();
+  });
 });
