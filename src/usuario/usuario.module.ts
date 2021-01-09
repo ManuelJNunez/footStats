60b2688e70769807e5607f3a9c000753a6422960
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-09 11:55:03 +0100

    fix: importado pgModule en UsuarioModule

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { EtcdService } from '../etcd/etcd.service';
+import { PgModule } from '../pg/pg.module';
 import { AuthGuard } from '../common/guards/auth.guard';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
+import { EtcdService } from '../etcd/etcd.service';
 
 @Module({
+  imports: [PgModule],
   controllers: [UsuarioController],
-  providers: [UsuarioService, AuthGuard],
+  providers: [UsuarioService, AuthGuard, EtcdService],
 })
 export class UsuarioModule {}
