188ab2b220d8fbdfd062fe59242b3e6b4c522930
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-11 19:10:23 +0100

    tests: añade providers (#5 #6)

--- src/partido/partido.controller.spec.ts
+++ src/partido/partido.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { EtcdService } from '../etcd/etcd.service';
+import { PgService } from '../pg/pg.service';
 import { PartidoController } from './partido.controller';
+import { PartidoService } from './partido.service';
 
 describe('PartidoController', () => {
   let controller: PartidoController;
@@ -7,6 +11,7 @@ describe('PartidoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PartidoController],
+      providers: [PartidoService, EtcdService, AuthGuard, PgService],
     }).compile();
 
     controller = module.get<PartidoController>(PartidoController);
