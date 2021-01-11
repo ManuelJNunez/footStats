26a756e7cb48d69294b7e2b9946f48396b77d6dc
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-11 19:01:19 +0100

    refactor: queries actualizadas

--- src/pg/pg.service.ts
+++ src/pg/pg.service.ts
@@ -30,19 +30,21 @@ export class PgService {
   private async createTables() {
     await this.pool.query(`
       CREATE TABLE IF NOT EXISTS users (
-        userId SERIAL PRIMARY KEY,
+        "userId" SERIAL PRIMARY KEY,
         email VARCHAR(50) NOT NULL,
         nickname VARCHAR(20) NOT NULL,
-        password VARCHAR(20) NOT NULL
+        password VARCHAR(100) NOT NULL
       )
     `);
 
     await this.pool.query(`
-    CREATE TABLE IF NOT EXISTS partidos (
-      matchID SERIAL PRIMARY KEY,
-      beginDate DATE NOT NULL DEFAULT CURRENT_DATE,
-      endDate DATE NOT NULL,
-      lugar VARCHAR(30)
+    CREATE TABLE IF NOT EXISTS matches (
+      "matchId" SERIAL PRIMARY KEY,
+      "horaIni" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      "horaFin" timestamp NOT NULL,
+      lugar VARCHAR(30),
+      "userId" INT,
+      CONSTRAINT userId FOREIGN KEY ("userId") REFERENCES users("userId")
     )
   `);
   }
