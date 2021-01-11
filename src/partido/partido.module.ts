a3bc004ce87ac11b28f7dd31eed2b2ece498ad65
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-11 12:40:09 +0100

    refactor: añade providers (#5 #6)

--- src/partido/partido.module.ts
+++ src/partido/partido.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { EtcdService } from '../etcd/etcd.service';
+import { PgService } from '../pg/pg.service';
 import { PartidoController } from './partido.controller';
+import { PartidoService } from './partido.service';
 
 @Module({
   controllers: [PartidoController],
+  providers: [PartidoService, PgService, EtcdService, AuthGuard],
 })
 export class PartidoModule {}
