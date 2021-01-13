1d26d85ea87ddaef27d0dd4fe92d770909e5b3c5
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-13 19:57:26 +0100

    refactor: añade modulo que contiene la SSOT (#5 #6)

--- src/partido/partido.module.ts
+++ src/partido/partido.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PgModule } from '../pg/pg.module';
 import { AuthGuard } from '../common/guards/auth.guard';
 import { EtcdService } from '../etcd/etcd.service';
-import { PgService } from '../pg/pg.service';
 import { PartidoController } from './partido.controller';
 import { PartidoService } from './partido.service';
 
 @Module({
+  imports: [PgModule],
   controllers: [PartidoController],
-  providers: [PartidoService, EtcdService, AuthGuard, PgService],
+  providers: [PartidoService, EtcdService, AuthGuard],
 })
 export class PartidoModule {}
