8d65486b404391d0507e234e433495d65df61721
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-01-14 17:51:06 +0100

    refactor: añadido puerto como variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log'],
   });
   app.useGlobalInterceptors(new LoggerInterceptor());
-  await app.listen(3000);
+  await app.listen(process.env.port || '3000');
 }
 bootstrap();
